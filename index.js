36d5e3448ad142ce0111c2a4c00701b1102328e5
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-18 17:14:07 -0400

    function to write readme

--- index.js
+++ index.js
@@ -13,10 +13,15 @@ const questions = [
     },
     // Add additional questions here
   ];
-  
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
+  const fs = require('fs');
+
+  function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('Success!')
+    );
+  }
+  
 // TODO: Create a function to initialize app
 function init() {}
 
