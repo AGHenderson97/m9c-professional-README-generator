4c34191ffaa48c427d888ce204f14c14a4fe052e
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-25 20:47:01 -0400

    added table of contents/Contributing/Test/Question

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,46 +1,34 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (!license) {
     return "";
   }
 
-  const badge = `![${license} license](https://img.shields.io/badge/license-${license}-brightgreen)`;
-
-  return badge;
+  return `![${license} license](https://img.shields.io/badge/license-${license}-brightgreen)`;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (!license) {
     return "";
   }
 
-  const link = `[${license}](https://opensource.org/licenses/${license})`;
-
-  return link;
+  return `[${license}](https://opensource.org/licenses/${license})`;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (!license) {
     return "";
   }
 
-  const section = `
+  return `
 ## License
 
 This project is licensed under the ${renderLicenseLink(license)} license.
 `;
-
-  return section;
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
 ${renderLicenseBadge(data.license)}
 
@@ -48,7 +36,36 @@ ${renderLicenseBadge(data.license)}
 
 ${data.description}
 
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
 ${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions about the project, please feel free to contact me through my [GitHub profile](https://github.com/${data.githubUsername}) or via email at ${data.email}.
 `;
 }
 
