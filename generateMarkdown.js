984ecfa14be2e80c3c6ce694a32706e6c22be34f
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-25 20:17:58 -0400

    added renderLicenseLink function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -12,7 +12,15 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+
+  const link = `[${license}](https://opensource.org/licenses/${license})`;
+
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
