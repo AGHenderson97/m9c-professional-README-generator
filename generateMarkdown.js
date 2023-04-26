d2dcd3eaae0685811626c92713666fde9dd64416
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-25 20:16:15 -0400

    added renderLicenseBadge function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+
+  const badge = `![${license} license](https://img.shields.io/badge/license-${license}-brightgreen)`;
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
