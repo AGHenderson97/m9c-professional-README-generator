2be7c5c85f65423e5e237325a98877c59179bed8
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-28 00:31:25 -0400

    syntax error

--- generateMarkdown.mjs
+++ generateMarkdown.mjs
@@ -1,14 +1,4 @@
-import { renderLicenseSection } from './utils/renderLicense.mjs';
-
-function generateMarkdown(data) {
-  return 
-
-  return `
-## License
-
-This project is licensed under the ${renderLicenseLink(license)} license. ${renderLicenseBadge(license)}
-`;
-}
+import { renderLicenseBadge, renderLicenseLink, renderLicenseSection } from './utils/renderLicense.mjs';
 
 function generateMarkdown(data) {
   return `
@@ -49,7 +39,12 @@ ${data.tests}
 
 ## Questions
 
-If you have any questions or comments about this project, please contact me at ${data.email}. You can also visit my [GitHub profile](https://github.com/${data.github}) for additional information and resources. Thank you!`;
+If you have any questions or comments about this project, please contact me at ${data.email}. You can also visit my [GitHub profile](https://github.com/${data.github}) for additional information and resources. Thank you!
+
+## License
+
+This project is licensed under the ${renderLicenseLink(data.license)} license. ${renderLicenseBadge(data.license)}
+`;
 }
 
 export default generateMarkdown;
