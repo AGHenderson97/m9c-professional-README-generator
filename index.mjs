84fdf027266fdf26647ed37f8c80874164c256c4
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-28 01:37:00 -0400

    working to create new folder/path generated README

--- index.mjs
+++ index.mjs
@@ -2,66 +2,68 @@ import inquirer from 'inquirer';
 import generateMarkdown from './generateMarkdown.mjs';
 import { writeToFile } from './utils/fs.mjs';
 
-const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Please provide a description of your project:',
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'Please provide installation instructions for your project:',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'Please provide usage information for your project:',
-  },
-  {
-    type: 'input',
-    name: 'contributing',
-    message: 'Please provide contribution guidelines for your project:',
-  },
-  {
-    type: 'input',
-    name: 'tests',
-    message: 'Please provide test instructions for your project:',
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'Please select a license for your project:',
-    choices: ['MIT', 'GPLv2', 'Apache', 'BSD', 'None'],
-  },
-  {
-    type: 'input',
-    name: 'github',
-    message: 'Please provide your GitHub username:',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'Please provide your email address:',
-  },
-];
+const path = './generated_readmes';
 
-function init() {
-  inquirer
-    .prompt(questions)
-    .then((answers) => {
-      const readmeContent = generateMarkdown(answers); // Generate README content
-      writeToFile('README.md', readmeContent); // Write README file
-      console.log('Successfully created README.md!');
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+async function init() {
+  try {
+    const responses = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage instructions for your project:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide contribution guidelines for your project:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions for your project:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license for your project:',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address:',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide your GitHub username:',
+      },
+    ]);
+
+    const filename = `${path}/MY-README.md`;
+
+    const markdown = generateMarkdown(responses);
+    await writeToFile(filename, markdown);
+
+    console.log(`Your README file has been generated and is located in ${filename}`);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 init();
